fe15a6c530ac7f5c16e3b65d84afdab9add7ffba
UnaMela29 <jeemi2981@gmail.com>
2023-04-09 21:54:28 +0900

    자주사용 플러그인까지

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options:{
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]',
+          limit: 20000, //20kb
+        },
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+      `
+    }),
+  ]
+}
